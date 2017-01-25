b69eb26b85f0751d10369b495762b8da2a53226c
David Allysson <davidmoreirainformatica@gmail.com>
2017-01-25 14:35:28 -0300

    movimentações concluidas

--- player.rb
+++ player.rb
@@ -13,7 +13,7 @@ class Player
 		@height = 14
 
 		@radius = 8 #valor fixo
-		@velocity = 6
+		@velocity = 4
 
 		#Indice referente a imagem inicial para o personagem
 		@image_index = 6
@@ -33,6 +33,42 @@ class Player
   def move_up
     @y -= @velocity
 		@y = 29 + @radius if @y < 29 + @radius #Limitador do movimento para cima
+
+		@y += @velocity if @y >= 56 and @y <= 71 and @x >= 35 and @x <= 59 #Limitação do movimento para cima - 1ª Linha 1° bloco
+		@y += @velocity if @y >= 56 and @y <= 71 and @x >= 67 and @x <= 91 #Limitação do movimento para cima - 1ª Linha 2° bloco
+		@y += @velocity if @y >= 56 and @y <= 71 and @x >= 99 and @x <= 123 #Limitação do movimento para cima - 1ª Linha 3° bloco
+		@y += @velocity if @y >= 56 and @y <= 71 and @x >= 131 and @x <= 155 #Limitação do movimento para cima - 1ª Linha 4° bloco
+		@y += @velocity if @y >= 56 and @y <= 71 and @x >= 163 and @x <= 187 #Limitação do movimento para cima - 1ª Linha 5° bloco
+		@y += @velocity if @y >= 56 and @y <= 71 and @x >= 195 and @x <= 219 #Limitação do movimento para cima - 1ª Linha 6° bloco
+
+		@y += @velocity if @y >= 88 and @y <= 103 and @x >= 35 and @x <= 59 #Limitação do movimento para cima - 2ª Linha 1° bloco
+		@y += @velocity if @y >= 88 and @y <= 103 and @x >= 67 and @x <= 91 #Limitação do movimento para cima - 2ª Linha 2° bloco
+		@y += @velocity if @y >= 88 and @y <= 103 and @x >= 99 and @x <= 123 #Limitação do movimento para cima - 2ª Linha 3° bloco
+		@y += @velocity if @y >= 88 and @y <= 103 and @x >= 131 and @x <= 155 #Limitação do movimento para cima - 2ª Linha 4° bloco
+		@y += @velocity if @y >= 88 and @y <= 103 and @x >= 163 and @x <= 187 #Limitação do movimento para cima - 2ª Linha 5° bloco
+		@y += @velocity if @y >= 88 and @y <= 103 and @x >= 195 and @x <= 219 #Limitação do movimento para cima - 2ª Linha 6° bloco
+
+		@y += @velocity if @y >= 110 and @y <= 135 and @x >= 35 and @x <= 59 #Limitação do movimento para cima - 3ª Linha 1° bloco
+		@y += @velocity if @y >= 110 and @y <= 135 and @x >= 67 and @x <= 91 #Limitação do movimento para cima - 3ª Linha 2° bloco
+		@y += @velocity if @y >= 110 and @y <= 135 and @x >= 99 and @x <= 123 #Limitação do movimento para cima - 3ª Linha 3° bloco
+		@y += @velocity if @y >= 110 and @y <= 135 and @x >= 131 and @x <= 155 #Limitação do movimento para cima - 3ª Linha 4° bloco
+		@y += @velocity if @y >= 110 and @y <= 135 and @x >= 163 and @x <= 187 #Limitação do movimento para cima - 3ª Linha 5° bloco
+		@y += @velocity if @y >= 110 and @y <= 135 and @x >= 195 and @x <= 219 #Limitação do movimento para cima - 3ª Linha 6° bloco
+
+		@y += @velocity if @y >= 142 and @y <= 167 and @x >= 35 and @x <= 59 #Limitação do movimento para cima - 4ª Linha 1° bloco
+		@y += @velocity if @y >= 142 and @y <= 167 and @x >= 67 and @x <= 91 #Limitação do movimento para cima - 4ª Linha 2° bloco
+		@y += @velocity if @y >= 142 and @y <= 167 and @x >= 99 and @x <= 123 #Limitação do movimento para cima - 4ª Linha 3° bloco
+		@y += @velocity if @y >= 142 and @y <= 167 and @x >= 131 and @x <= 155 #Limitação do movimento para cima - 4ª Linha 4° bloco
+		@y += @velocity if @y >= 142 and @y <= 167 and @x >= 163 and @x <= 187 #Limitação do movimento para cima - 4ª Linha 5° bloco
+		@y += @velocity if @y >= 142 and @y <= 167 and @x >= 195 and @x <= 219 #Limitação do movimento para cima - 4ª Linha 6° bloco
+
+		@y += @velocity if @y >= 174 and @y <= 199 and @x >= 35 and @x <= 59 #Limitação do movimento para cima - 5ª Linha 1° bloco
+		@y += @velocity if @y >= 174 and @y <= 199 and @x >= 67 and @x <= 91 #Limitação do movimento para cima - 5ª Linha 2° bloco
+		@y += @velocity if @y >= 174 and @y <= 199 and @x >= 99 and @x <= 123 #Limitação do movimento para cima - 5ª Linha 3° bloco
+		@y += @velocity if @y >= 174 and @y <= 199 and @x >= 131 and @x <= 155 #Limitação do movimento para cima - 5ª Linha 4° bloco
+		@y += @velocity if @y >= 174 and @y <= 199 and @x >= 163 and @x <= 187 #Limitação do movimento para cima - 5ª Linha 5° bloco
+		@y += @velocity if @y >= 174 and @y <= 199 and @x >= 195 and @x <= 219 #Limitação do movimento para cima - 5ª Linha 6° bloco
+
 		changeSprite([0, 1, 2])
   end
 
@@ -40,6 +76,42 @@ class Player
   def move_down
 		@y += @velocity
 		@y = (@window.height - @radius - 35) if @y > @window.height - @radius - 35 #Limitador do movimento para baixo
+
+		@y -= @velocity if @y >= 40 and @y <= 56 and @x >= 35 and @x <= 59 #Limitação do movimento para baixo - 1ª Linha 1° bloco
+		@y -= @velocity if @y >= 40 and @y <= 56 and @x >= 67 and @x <= 91 #Limitação do movimento para baixo - 1ª Linha 2° bloco
+		@y -= @velocity if @y >= 40 and @y <= 56 and @x >= 99 and @x <= 123 #Limitação do movimento para baixo - 1ª Linha 3° bloco
+		@y -= @velocity if @y >= 40 and @y <= 56 and @x >= 131 and @x <= 155 #Limitação do movimento para baixo - 1ª Linha 4° bloco
+		@y -= @velocity if @y >= 40 and @y <= 56 and @x >= 163 and @x <= 187 #Limitação do movimento para baixo - 1ª Linha 5° bloco
+		@y -= @velocity if @y >= 40 and @y <= 56 and @x >= 195 and @x <= 219 #Limitação do movimento para baixo - 1ª Linha 6° bloco
+
+		@y -= @velocity if @y >= 72 and @y <= 88 and @x >= 35 and @x <= 59 #Limitação do movimento para baixo - 2ª Linha 1° bloco
+		@y -= @velocity if @y >= 72 and @y <= 88 and @x >= 67 and @x <= 91 #Limitação do movimento para baixo - 2ª Linha 2° bloco
+		@y -= @velocity if @y >= 72 and @y <= 88 and @x >= 99 and @x <= 123 #Limitação do movimento para baixo - 2ª Linha 3° bloco
+		@y -= @velocity if @y >= 72 and @y <= 88 and @x >= 131 and @x <= 155 #Limitação do movimento para baixo - 2ª Linha 4° bloco
+		@y -= @velocity if @y >= 72 and @y <= 88 and @x >= 163 and @x <= 187 #Limitação do movimento para baixo - 2ª Linha 5° bloco
+		@y -= @velocity if @y >= 72 and @y <= 88 and @x >= 195 and @x <= 219 #Limitação do movimento para baixo - 2ª Linha 6° bloco
+
+		@y -= @velocity if @y >= 104 and @y <= 120 and @x >= 35 and @x <= 59 #Limitação do movimento para baixo - 3ª Linha 1° bloco
+		@y -= @velocity if @y >= 104 and @y <= 120 and @x >= 67 and @x <= 91 #Limitação do movimento para baixo - 3ª Linha 2° bloco
+		@y -= @velocity if @y >= 104 and @y <= 120 and @x >= 99 and @x <= 123 #Limitação do movimento para baixo - 3ª Linha 3° bloco
+		@y -= @velocity if @y >= 104 and @y <= 120 and @x >= 131 and @x <= 155 #Limitação do movimento para baixo - 3ª Linha 4° bloco
+		@y -= @velocity if @y >= 104 and @y <= 120 and @x >= 163 and @x <= 187 #Limitação do movimento para baixo - 3ª Linha 5° bloco
+		@y -= @velocity if @y >= 104 and @y <= 120 and @x >= 195 and @x <= 219 #Limitação do movimento para baixo - 3ª Linha 6° bloco
+
+		@y -= @velocity if @y >= 136 and @y <= 152 and @x >= 35 and @x <= 59 #Limitação do movimento para baixo - 4ª Linha 1° bloco
+		@y -= @velocity if @y >= 136 and @y <= 152 and @x >= 67 and @x <= 91 #Limitação do movimento para baixo - 4ª Linha 2° bloco
+		@y -= @velocity if @y >= 136 and @y <= 152 and @x >= 99 and @x <= 123 #Limitação do movimento para baixo - 4ª Linha 3° bloco
+		@y -= @velocity if @y >= 136 and @y <= 152 and @x >= 131 and @x <= 155 #Limitação do movimento para baixo - 4ª Linha 4° bloco
+		@y -= @velocity if @y >= 136 and @y <= 152 and @x >= 163 and @x <= 187 #Limitação do movimento para baixo - 4ª Linha 5° bloco
+		@y -= @velocity if @y >= 136 and @y <= 152 and @x >= 195 and @x <= 219 #Limitação do movimento para baixo - 4ª Linha 6° bloco
+
+		@y -= @velocity if @y >= 168 and @y <= 184 and @x >= 35 and @x <= 59 #Limitação do movimento para baixo - 5ª Linha 1° bloco
+		@y -= @velocity if @y >= 168 and @y <= 184 and @x >= 67 and @x <= 91 #Limitação do movimento para baixo - 5ª Linha 2° bloco
+		@y -= @velocity if @y >= 168 and @y <= 184 and @x >= 99 and @x <= 123 #Limitação do movimento para baixo - 5ª Linha 3° bloco
+		@y -= @velocity if @y >= 168 and @y <= 184 and @x >= 131 and @x <= 155 #Limitação do movimento para baixo - 5ª Linha 4° bloco
+		@y -= @velocity if @y >= 168 and @y <= 184 and @x >= 163 and @x <= 187 #Limitação do movimento para baixo - 5ª Linha 5° bloco
+		@y -= @velocity if @y >= 168 and @y <= 184 and @x >= 195 and @x <= 219 #Limitação do movimento para baixo - 5ª Linha 6° bloco
+
 		changeSprite([6, 7, 8])
   end
 
@@ -47,6 +119,42 @@ class Player
   def move_left
 		@x -= @velocity
 		@x = 23 + @radius if @x < 23 + @radius #Limitador do movimento para a esquerda
+
+		@x += @velocity if @y >= 40 and @y <= 62 and @x >= 35 and @x <= 59 #Limitação do movimento para a esquerda - 1ª Linha 1° bloco
+		@x += @velocity if @y >= 40 and @y <= 62 and @x >= 67 and @x <= 91 #Limitação do movimento para a esquerda - 1ª Linha 2° bloco
+		@x += @velocity if @y >= 40 and @y <= 62 and @x >= 99 and @x <= 123 #Limitação do movimento para a esquerda - 1ª Linha 3° bloco
+		@x += @velocity if @y >= 40 and @y <= 62 and @x >= 131 and @x <= 155 #Limitação do movimento para a esquerda - 1ª Linha 4° bloco
+		@x += @velocity if @y >= 40 and @y <= 62 and @x >= 163 and @x <= 187 #Limitação do movimento para a esquerda - 1ª Linha 5° bloco
+		@x += @velocity if @y >= 40 and @y <= 62 and @x >= 195 and @x <= 219 #Limitação do movimento para a esquerda - 1ª Linha 6° bloco
+
+		@x += @velocity if @y >= 72 and @y <= 94 and @x >= 35 and @x <= 59 #Limitação do movimento para a esquerda - 2ª Linha 1° bloco
+		@x += @velocity if @y >= 72 and @y <= 94 and @x >= 67 and @x <= 91 #Limitação do movimento para a esquerda - 2ª Linha 2° bloco
+		@x += @velocity if @y >= 72 and @y <= 94 and @x >= 99 and @x <= 123 #Limitação do movimento para a esquerda - 2ª Linha 3° bloco
+		@x += @velocity if @y >= 72 and @y <= 94 and @x >= 131 and @x <= 155 #Limitação do movimento para a esquerda - 2ª Linha 4° bloco
+		@x += @velocity if @y >= 72 and @y <= 94 and @x >= 163 and @x <= 187 #Limitação do movimento para a esquerda - 2ª Linha 5° bloco
+		@x += @velocity if @y >= 72 and @y <= 94 and @x >= 195 and @x <= 219 #Limitação do movimento para a esquerda - 2ª Linha 6° bloco
+
+		@x += @velocity if @y >= 104 and @y <= 126 and @x >= 35 and @x <= 59 #Limitação do movimento para a esquerda - 3ª Linha 1° bloco
+		@x += @velocity if @y >= 104 and @y <= 126 and @x >= 67 and @x <= 91 #Limitação do movimento para a esquerda - 3ª Linha 2° bloco
+		@x += @velocity if @y >= 104 and @y <= 126 and @x >= 99 and @x <= 123 #Limitação do movimento para a esquerda - 3ª Linha 3° bloco
+		@x += @velocity if @y >= 104 and @y <= 126 and @x >= 131 and @x <= 155 #Limitação do movimento para a esquerda - 3ª Linha 4° bloco
+		@x += @velocity if @y >= 104 and @y <= 126 and @x >= 163 and @x <= 187 #Limitação do movimento para a esquerda - 3ª Linha 5° bloco
+		@x += @velocity if @y >= 104 and @y <= 126 and @x >= 195 and @x <= 219 #Limitação do movimento para a esquerda - 3ª Linha 6° bloco
+
+		@x += @velocity if @y >= 136 and @y <= 158 and @x >= 35 and @x <= 59 #Limitação do movimento para a esquerda - 4ª Linha 1° bloco
+		@x += @velocity if @y >= 136 and @y <= 158 and @x >= 67 and @x <= 91 #Limitação do movimento para a esquerda - 4ª Linha 2° bloco
+		@x += @velocity if @y >= 136 and @y <= 158 and @x >= 99 and @x <= 123 #Limitação do movimento para a esquerda - 4ª Linha 3° bloco
+		@x += @velocity if @y >= 136 and @y <= 158 and @x >= 131 and @x <= 155 #Limitação do movimento para a esquerda - 4ª Linha 4° bloco
+		@x += @velocity if @y >= 136 and @y <= 158 and @x >= 163 and @x <= 187 #Limitação do movimento para a esquerda - 4ª Linha 5° bloco
+		@x += @velocity if @y >= 136 and @y <= 158 and @x >= 195 and @x <= 219 #Limitação do movimento para a esquerda - 4ª Linha 6° bloco
+
+		@x += @velocity if @y >= 168 and @y <= 190 and @x >= 35 and @x <= 59 #Limitação do movimento para a esquerda - 5ª Linha 1° bloco
+		@x += @velocity if @y >= 168 and @y <= 190 and @x >= 67 and @x <= 91 #Limitação do movimento para a esquerda - 5ª Linha 2° bloco
+		@x += @velocity if @y >= 168 and @y <= 190 and @x >= 99 and @x <= 123 #Limitação do movimento para a esquerda - 5ª Linha 3° bloco
+		@x += @velocity if @y >= 168 and @y <= 190 and @x >= 131 and @x <= 155 #Limitação do movimento para a esquerda - 5ª Linha 4° bloco
+		@x += @velocity if @y >= 168 and @y <= 190 and @x >= 163 and @x <= 187 #Limitação do movimento para a esquerda - 5ª Linha 5° bloco
+		@x += @velocity if @y >= 168 and @y <= 190 and @x >= 195 and @x <= 219 #Limitação do movimento para a esquerda - 5ª Linha 6° bloco
+
 		changeSprite([9, 10, 11])
   end
 
@@ -54,6 +162,44 @@ class Player
   def move_right
 		@x += @velocity
 		@x = (@window.width - @radius - 25) if @x > @window.width - @radius - 25 #Limitador do movimento para a direita
+
+		@x -= @velocity if @y >= 40 and @y <= 62 and @x >= 35 and @x <= 59 #Limitação do movimento para a direita - 1ª Linha 1° bloco
+		@x -= @velocity if @y >= 40 and @y <= 62 and @x >= 67 and @x <= 91 #Limitação do movimento para a direita - 1ª Linha 2° bloco
+		@x -= @velocity if @y >= 40 and @y <= 62 and @x >= 99 and @x <= 123 #Limitação do movimento para a direita - 1ª Linha 3° bloco
+		@x -= @velocity if @y >= 40 and @y <= 62 and @x >= 131 and @x <= 155 #Limitação do movimento para a direita - 1ª Linha 4° bloco
+		@x -= @velocity if @y >= 40 and @y <= 62 and @x >= 163 and @x <= 187 #Limitação do movimento para a direita - 1ª Linha 5° bloco
+		@x -= @velocity if @y >= 40 and @y <= 62 and @x >= 195 and @x <= 219 #Limitação do movimento para a direita - 1ª Linha 6° bloco
+
+		@x -= @velocity if @y >= 72 and @y <= 94 and @x >= 35 and @x <= 59 #Limitação do movimento para a direita - 2ª Linha 1° bloco
+		@x -= @velocity if @y >= 72 and @y <= 94 and @x >= 67 and @x <= 91 #Limitação do movimento para a direita - 2ª Linha 2° bloco
+		@x -= @velocity if @y >= 72 and @y <= 94 and @x >= 99 and @x <= 123 #Limitação do movimento para a direita - 2ª Linha 3° bloco
+		@x -= @velocity if @y >= 72 and @y <= 94 and @x >= 131 and @x <= 155 #Limitação do movimento para a direita - 2ª Linha 4° bloco
+		@x -= @velocity if @y >= 72 and @y <= 94 and @x >= 163 and @x <= 187 #Limitação do movimento para a direita - 2ª Linha 5° bloco
+		@x -= @velocity if @y >= 72 and @y <= 94 and @x >= 195 and @x <= 219 #Limitação do movimento para a direita - 2ª Linha 6° bloco
+
+		@x -= @velocity if @y >= 104 and @y <= 126 and @x >= 35 and @x <= 59 #Limitação do movimento para a direita - 3ª Linha 1° bloco
+		@x -= @velocity if @y >= 104 and @y <= 126 and @x >= 67 and @x <= 91 #Limitação do movimento para a direita - 3ª Linha 2° bloco
+		@x -= @velocity if @y >= 104 and @y <= 126 and @x >= 99 and @x <= 123 #Limitação do movimento para a direita - 3ª Linha 3° bloco
+		@x -= @velocity if @y >= 104 and @y <= 126 and @x >= 131 and @x <= 155 #Limitação do movimento para a direita - 3ª Linha 4° bloco
+		@x -= @velocity if @y >= 104 and @y <= 126 and @x >= 163 and @x <= 187 #Limitação do movimento para a direita - 3ª Linha 5° bloco
+		@x -= @velocity if @y >= 104 and @y <= 126 and @x >= 195 and @x <= 219 #Limitação do movimento para a direita - 3ª Linha 6° bloco
+
+		@x -= @velocity if @y >= 136 and @y <= 158 and @x >= 35 and @x <= 59 #Limitação do movimento para a direita - 4ª Linha 1° bloco
+		@x -= @velocity if @y >= 136 and @y <= 158 and @x >= 67 and @x <= 91 #Limitação do movimento para a direita - 4ª Linha 2° bloco
+		@x -= @velocity if @y >= 136 and @y <= 158 and @x >= 99 and @x <= 123 #Limitação do movimento para a direita - 4ª Linha 3° bloco
+		@x -= @velocity if @y >= 136 and @y <= 158 and @x >= 131 and @x <= 155 #Limitação do movimento para a direita - 4ª Linha 4° bloco
+		@x -= @velocity if @y >= 136 and @y <= 158 and @x >= 163 and @x <= 187 #Limitação do movimento para a direita - 4ª Linha 5° bloco
+		@x -= @velocity if @y >= 136 and @y <= 158 and @x >= 195 and @x <= 219 #Limitação do movimento para a direita - 4ª Linha 6° bloco
+
+		@x -= @velocity if @y >= 168 and @y <= 190 and @x >= 35 and @x <= 59 #Limitação do movimento para a direita - 5ª Linha 1° bloco
+		@x -= @velocity if @y >= 168 and @y <= 190 and @x >= 67 and @x <= 91 #Limitação do movimento para a direita - 5ª Linha 2° bloco
+		@x -= @velocity if @y >= 168 and @y <= 190 and @x >= 99 and @x <= 123 #Limitação do movimento para a direita - 5ª Linha 3° bloco
+		@x -= @velocity if @y >= 168 and @y <= 190 and @x >= 131 and @x <= 155 #Limitação do movimento para a direita - 5ª Linha 4° bloco
+		@x -= @velocity if @y >= 168 and @y <= 190 and @x >= 163 and @x <= 187 #Limitação do movimento para a direita - 5ª Linha 5° bloco
+		@x -= @velocity if @y >= 168 and @y <= 190 and @x >= 195 and @x <= 219 #Limitação do movimento para a direita - 5ª Linha 6° bloco
+
+
+
 		changeSprite([3, 4, 5])
   end
 
